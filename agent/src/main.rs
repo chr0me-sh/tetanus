701a4a7ece61aea4f6da8f54cd7a91f9dc85f3b9
Lachlan Barker <lachlan@chr0me.sh>
2020-11-11 12:52:24 +0800

    Final change to arg parsing for now

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -2,6 +2,7 @@ mod util;
 
 use std::env;
 use std::process::exit;
+use std::str::FromStr;
 use std::net;
 
 fn notify_exit(msg: &str) {
@@ -9,25 +10,29 @@ fn notify_exit(msg: &str) {
     exit(1)
 }
 
+fn parse_arg<T: FromStr>(n: usize) -> Option<T> {
+    match env::args().nth(n) {
+        Some(opt) => match opt.parse::<T>() {
+            Ok(res) => Some(res),
+            Err(_)  => None
+        }
+        None => None,
+    }
+}
+
 fn main() {
     if env::args().len() < 3 {
         notify_exit("Usage: ./agent [ip] [port]")
     } 
 
-    let cnc_ip: net::IpAddr = match env::args().nth(1) {
-        Some(ip_str) => match ip_str.parse() {
-                Ok(ip) => ip,
-                Err(_) => return notify_exit("Failed to parse IP address")
-            }
-        None => panic!("Failed to get address argument")
+    let cnc_ip: net::IpAddr = match parse_arg(1) {
+        Some(i) => i,
+        None    => return notify_exit("Failed to parse address")
     };
 
-    let cnc_port: u16 = match env::args().nth(2) {
-        Some(p_str) => match p_str.parse() {
-            Ok(p)  => p,
-            Err(_) => return notify_exit("Failed to parse port number")
-        }
-        None => panic!("Failed to get port argument")
+    let cnc_port: u16 = match parse_arg(2) {
+        Some(p) => p,
+        None    => return notify_exit("Failed to parse port")
     };
 
     println!("{:?}:{:?}", cnc_ip, cnc_port);
