82cb712be0f245cf9805ac865009cfe849848a08
Lachlan Barker <lachlan@chr0me.sh>
2020-11-13 15:50:14 +0800

    Added custom error w/ associated macros + impls

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,41 @@
 use std::net;
+use std::io;
+use std::num;
+use std::fmt;
+use std::convert::From;
 
-mod msg {
+macro_rules! cast_err {
+    ( $err:ty, $src:ty, $dst:expr) => (
+        impl From<$src> for $err {
+            fn from(e: $src) -> Self { $dst(e) }
+        }
+    )
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Argument,
+    Io(io::Error),
+    ParseAddr(net::AddrParseError),
+    ParseInt(num::ParseIntError)
+}
+
+cast_err!(Error, io::Error, Error::Io);
+cast_err!(Error, net::AddrParseError, Error::ParseAddr);
+cast_err!(Error, num::ParseIntError, Error::ParseInt);
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Error::Io(e)        => write!(f, "IO error: {}", e),
+            Error::Argument     => write!(f, "Usage: ./agent [ip] [port]"),
+            Error::ParseAddr(e) => write!(f, "Address parse error: {}", e),
+            Error::ParseInt(e)  => write!(f, "Integer parse error: {}", e),
+        }
+    }
+}
+
+pub mod msg {
     const MSG_HEADER: [u8; 4] = [84, 84, 78, 83];
 
     pub struct Message {
