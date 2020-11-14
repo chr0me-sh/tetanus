7a03918e1b383bf8917c5016e92d419e436d1c4a
Lachlan Barker <lachlan@chr0me.sh>
2020-11-14 11:36:24 +0800

    Initial commit for C2 Server

--- cnc/src/main.rs
+++ cnc/src/main.rs
@@ -1,3 +1,19 @@
+extern crate tetanus;
+
+use std::process::exit;
+
+fn run() -> Result<(), tetanus::Error> {
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = run();
+
+    match result {
+        Ok(_)  => exit(0),
+        Err(e) => {
+            eprintln!("{}", e);
+            exit(1)
+        }
+    }
 }
