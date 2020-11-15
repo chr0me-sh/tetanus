e7aec10ddeec3d3286a9d215b992212975ef1e76
Lachlan Barker <lachlan@chr0me.sh>
2020-11-15 11:22:49 +0800

    Added constructor, register, search methods to Menu

--- cnc/src/menu.rs
+++ cnc/src/menu.rs
@@ -1,10 +1,33 @@
 use std::net; 
+use std::collections::HashMap;
 
-pub struct Command<T> {
-    name:    String,
-    usage:   String,
-    nargs:   usize,
-    handler: fn()
+pub struct Menu {
+    prompt:  String,
+    options: HashMap<String, Command>,
+}
+
+impl Menu {
+    pub fn new<S: Into<String>>(prompt: S, options: HashMap<String, Command>) -> Menu {
+        Menu { prompt: prompt.into(), options }
+    }
+
+    pub fn empty<S: Into<String>>(prompt: S) -> Menu {
+        Menu::new(prompt, HashMap::new())
+    }
+
+    pub fn register(&mut self, cmd: Command) -> () {
+        self.options.insert(cmd.name.clone(), cmd);
+    }
+
+    pub fn search<S: Into<String>>(&self, cmd: S) -> Option<&Command> {
+        self.options.get(&cmd.into())
+    }
+}
+
+pub struct Command {
+    pub name:   String,
+    usage:  String,
+    nargs:  usize,
 }
 
 impl Command {
@@ -21,7 +44,7 @@ pub trait ArgParse<T> {
     fn parse(&self, cmd: Vec<&str>) -> Option<T>;
 }
 
-impl ArgParse<net::SocketAddr> for Command<Listener> {
+impl ArgParse<net::SocketAddr> for Command {
     // This kind of sucks
     fn parse(&self, cmd: Vec<&str>) -> Option<net::SocketAddr> {
         if cmd[1..].len() != self.nargs { return None }
